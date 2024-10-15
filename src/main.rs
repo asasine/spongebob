24d5ff2c6618d3107bd6e537ad643e16ba7eb5e2
PlexSheep <software@cscherr.de>
2024-10-15 21:57:50 +0200

    test(stdin): add unit tests regarding stdin inputs for spongebob binary
    Refs: #13

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,25 @@ mod tests {
     use arboard::Clipboard;
     use assert_cmd::Command;
 
+    const LONG_TEST_TEXT: &str = r"
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+    in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+    sunt in culpa qui officia deserunt mollit anim id est laborum.
+    ";
+    const LONG_TEST_TEXT_SPONGEBOB: &str = r"
+    LoReM iPsUm DoLoR sIt AmEt, CoNsEcTeTuR aDiPiScInG eLiT, sEd Do
+    EiUsMoD tEmPoR iNcIdIdUnT uT lAbOrE eT dOlOrE mAgNa AlIqUa. Ut
+    EnIm Ad MiNiM vEnIaM, qUiS nOsTrUd ExErCiTaTiOn UlLaMcO lAbOrIs
+    NiSi Ut AlIqUiP eX eA cOmMoDo CoNsEqUaT. dUiS aUtE iRuRe DoLoR
+    iN rEpReHeNdErIt In VoLuPtAtE vElIt EsSe CiLlUm DoLoRe Eu FuGiAt
+    NuLlA pArIaTuR. eXcEpTeUr SiNt OcCaEcAt CuPiDaTaT nOn PrOiDeNt,
+    SuNt In CuLpA qUi OfFiCiA dEsErUnT mOlLiT aNiM iD eSt LaBoRuM.
+    ";
+
     #[test]
     fn test_alternate_flag() {
         Command::cargo_bin("spongebob")
@@ -147,5 +166,55 @@ mod tests {
             clipboard.get_text().is_err(),
             "Clipboard should still be empty"
         );
+    }
+
+    #[test]
+    fn test_implicit_short_stdin() {
+        Command::cargo_bin("spongebob")
+            .unwrap()
+            .arg("--no-copy")
+            .arg("--alternate")
+            .write_stdin("This is a test")
+            .assert()
+            .success()
+            .stdout(predicates::str::contains("ThIs Is A tEsT"));
+    }
+
+    #[test]
+    fn test_explicit_short_stdin() {
+        Command::cargo_bin("spongebob")
+            .unwrap()
+            .arg("--no-copy")
+            .arg("--alternate")
+            .arg("-")
+            .write_stdin("This is a test")
+            .assert()
+            .success()
+            .stdout(predicates::str::contains("ThIs Is A tEsT"));
+    }
+
+    #[test]
+    fn test_explicit_long_stdin() {
+        Command::cargo_bin("spongebob")
+            .unwrap()
+            .arg("--no-copy")
+            .arg("--alternate")
+            .arg("-")
+            .write_stdin(LONG_TEST_TEXT)
+            .assert()
+            .success()
+            .stdout(predicates::str::contains(LONG_TEST_TEXT_SPONGEBOB));
+    }
+
+    #[test]
+    fn test_implicit_long_stdin() {
+        Command::cargo_bin("spongebob")
+            .unwrap()
+            .arg("--no-copy")
+            .arg("--alternate")
+            .write_stdin(LONG_TEST_TEXT)
+            .assert()
+            .success()
+            .stdout(predicates::str::contains(LONG_TEST_TEXT_SPONGEBOB));
     }
 }
